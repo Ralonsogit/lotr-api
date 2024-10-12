363b0f78b057238e7398cdf933ced9a47599f8de
Roberto Alonso <ralonsoalvarezr@gmail.com>
2024-10-12 18:58:38 +0200

    Add routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -16,3 +16,42 @@ use App\Http\Controllers\FactionController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+// Auth
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+// Add Middlewares
+Route::prefix('admin')->middleware(['auth:sanctum', 'checkAdmin'])->group(function () {
+    // Users
+    Route::get('/user', [AuthController::class, 'user']);
+
+    // Factions
+    Route::post('/factions', [FactionController::class, 'store']);
+    Route::put('/factions/{id}', [FactionController::class, 'update']);
+    Route::delete('/factions/{id}', [FactionController::class, 'destroy']);
+
+    // Equipments
+    Route::post('/equipments', [EquipmentController::class, 'store']);
+    Route::put('/equipments/{id}', [EquipmentController::class, 'update']);
+    Route::delete('/equipments/{id}', [EquipmentController::class, 'destroy']);
+
+    // Characters
+    Route::post('/characters', [CharacterController::class, 'store']);
+    Route::put('/characters/{id}', [CharacterController::class, 'update']);
+    Route::delete('/characters/{id}', [CharacterController::class, 'destroy']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Factions
+    Route::get('/factions', [FactionController::class, 'index']);
+
+    // Equipments
+    Route::get('/equipments', [EquipmentController::class, 'index']);
+
+    // Characters
+    Route::get('/characters', [CharacterController::class, 'index']);
+});
