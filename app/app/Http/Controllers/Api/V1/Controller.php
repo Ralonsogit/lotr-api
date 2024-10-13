c25c8e8ab58422959aa6f4a90e2056ca9b81845a
Roberto Alonso <ralonsoalvarezr@gmail.com>
2024-10-13 11:49:32 +0200

    Add api versioning

--- app/app/Http/Controllers/Api/V1/Controller.php
+++ app/app/Http/Controllers/Api/V1/Controller.php
@@ -1,12 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
-use App\Exceptions\ApiException;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
--- app/routes/api.php
+++ app/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CharacterController;
-use App\Http\Controllers\EquipmentController;
-use App\Http\Controllers\FactionController;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\CharacterController;
+use App\Http\Controllers\Api\V1\EquipmentController;
+use App\Http\Controllers\Api\V1\FactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,41 +17,44 @@ use App\Http\Controllers\FactionController;
 |
 */
 
-// Auth
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-
-// Add Middlewares
-Route::prefix('admin')->middleware(['auth:sanctum', 'checkAdmin'])->group(function () {
-    // Factions
-    Route::post('/factions', [FactionController::class, 'store']);
-    Route::put('/factions/{id}', [FactionController::class, 'update']);
-    Route::delete('/factions/{id}', [FactionController::class, 'destroy']);
-
-    // Equipments
-    Route::post('/equipments', [EquipmentController::class, 'store']);
-    Route::put('/equipments/{id}', [EquipmentController::class, 'update']);
-    Route::delete('/equipments/{id}', [EquipmentController::class, 'destroy']);
-
-    // Characters
-    Route::post('/characters', [CharacterController::class, 'store']);
-    Route::put('/characters/{id}', [CharacterController::class, 'update']);
-    Route::delete('/characters/{id}', [CharacterController::class, 'destroy']);
-});
-
-Route::middleware('auth:sanctum')->group(function () {
+// Api V1
+Route::prefix('v1')->group(function() {
     // Auth
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
-    // Factions
-    Route::get('/factions', [FactionController::class, 'index']);
-    Route::get('/factions/{id}', [FactionController::class, 'show']);
+    // Add Middlewares
+    Route::prefix('admin')->middleware(['auth:sanctum', 'checkAdmin'])->group(function () {
+        // Factions
+        Route::post('/factions', [FactionController::class, 'store']);
+        Route::put('/factions/{id}', [FactionController::class, 'update']);
+        Route::delete('/factions/{id}', [FactionController::class, 'destroy']);
 
-    // Equipments
-    Route::get('/equipments', [EquipmentController::class, 'index']);
-    Route::get('/equipments/{id}', [EquipmentController::class, 'show']);
+        // Equipments
+        Route::post('/equipments', [EquipmentController::class, 'store']);
+        Route::put('/equipments/{id}', [EquipmentController::class, 'update']);
+        Route::delete('/equipments/{id}', [EquipmentController::class, 'destroy']);
 
-    // Characters
-    Route::get('/characters', [CharacterController::class, 'index']);
-    Route::get('/characters/{id}', [CharacterController::class, 'show']);
+        // Characters
+        Route::post('/characters', [CharacterController::class, 'store']);
+        Route::put('/characters/{id}', [CharacterController::class, 'update']);
+        Route::delete('/characters/{id}', [CharacterController::class, 'destroy']);
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        // Auth
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+        // Factions
+        Route::get('/factions', [FactionController::class, 'index']);
+        Route::get('/factions/{id}', [FactionController::class, 'show']);
+
+        // Equipments
+        Route::get('/equipments', [EquipmentController::class, 'index']);
+        Route::get('/equipments/{id}', [EquipmentController::class, 'show']);
+
+        // Characters
+        Route::get('/characters', [CharacterController::class, 'index']);
+        Route::get('/characters/{id}', [CharacterController::class, 'show']);
+    });
 });
